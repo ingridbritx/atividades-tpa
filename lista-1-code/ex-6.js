423d9cd81c8a55b2a212f48eeff932831ecda264
ingridbritx <iplb1803@gmail.com>
2025-08-20 20:13:16 -0300

    Update ex-6.js

--- lista-1-code/ex-6.js
+++ lista-1-code/ex-6.js
@@ -1,11 +1,12 @@
 const input = require ('readline-sync')
 let numero1 = input.questionInt ("Digite o primeiro numero: ");
-let numero2 = input.questionInt ("Digite o segunfo numero: ");
+let numero2 = input.questionInt ("Digite o segundo numero: ");
 
 let resultado1 = numero1 - 3;
-let resultado = numero2 +2;
+let resultado2 = numero2 +2;
 let soma = resultado1 + resultado2;
 let final = soma - 1; 
 
 console.log("O resultado final é:", final);
+
 
