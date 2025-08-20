c17ec6093f149bd9dd22eb5277bba592ca55e1c5
ingridbritx <iplb1803@gmail.com>
2025-08-20 19:54:01 -0300

    Update ex-3.js

--- lista-1-code/ex-3.js
+++ lista-1-code/ex-3.js
@@ -1,7 +1,9 @@
 const input = require ('readline-sync')
 let numero1 = input.questionInt ('Digite o primeiro numero: ');
 let numero2 = input.questionInt ('Digite o segundo numero: ');
-let resultado = numero1 + numero2; 
-console.log ("O resultado é:", resultado); 
+let resultado1 = numero1 + 2; 
+let resultado2 = numero2 + 2; 
+console.log ("O primeiro resultado é:", resultado1); 
+console.log ("O segundo resultado é:", resultado2); 
 
 
