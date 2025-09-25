576186df9644ee6b6937e5cfc73522bf6aafbf35
ingridbritx <iplb1803@gmail.com>
2025-09-24 21:54:25 -0300

    Add files via upload

---
+++ laço-de-repetição/exemplo1.js
@@ -0,0 +1,8 @@
+let biscoitos=1;
+
+while (biscoitos<=10){
+    console.log(`E como o ${biscoitos}º biscoito`); 
+    biscoitos++; 
+}
+
+console.log("Acabou os biscoitos");
---
+++ laço-de-repetição/tabuada.js
@@ -0,0 +1,9 @@
+let tabuada=8;
+let contador=1;
+
+while (contador<=10){
+    console.log(`A respostá é ${tabuada} x ${contador} = ${tabuada * contador}`);
+    contador++;
+}
+
+console.log("Acabou a tabuada")
