30fb23fd7d106a2351e93787800075f7d778ee9e
ingridbritx <iplb1803@gmail.com>
2025-11-05 19:56:35 -0300

    Add files via upload

---
+++ javascript- intermediário/aula.js
@@ -0,0 +1,57 @@
+//let frutas = ['maça', 'banana', 'laranja', 'uva'];
+//frutas.push("goiaba")
+//frutas.unshift("goiaba")
+
+//console.log(frutas);
+
+//frutas.pop() 
+
+//frutas.shift()
+//console.log(frutas);
+
+//console.log(typeof frutas);
+
+//const produto ={
+//    nome:"Coxinha", 
+//    preco: 6.0, 
+//    emEstoque: true
+//};
+
+//console.log(produto.nome);
+//console.log(produto["preco"]);
+//delete produto .emEstoque
+//console.log(produto);
+
+//produto.categoria = "Salgado"
+
+//console.log(produto);
+
+//function somaDoisNumerosInteiros(1,2) {
+//  return numero1 + numero2;
+//}
+
+//let resultado = somaDoisNumerosInteiros (1,2)
+
+//let soma = function(numero1,numero2){
+//    return numero1+numero2
+//}
+ 
+//let somaArrowFuction = (numero1, numero2) =>{
+//    return numero1+numero2
+//}
+
+//console.log(somaArrowFuction(1,9));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
